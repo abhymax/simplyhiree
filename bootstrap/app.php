dbcae724a0639e95da0aacd247efa5eb94a07be3
ABHINAB <abhinab@ABHINABs-MacBook-Air.local>
2026-02-13 09:22:58 +0530

    Register status.check middleware alias

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Middleware\CheckAccountStatus;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -12,7 +13,9 @@ return Application::configure(dirname(__DIR__))
         require base_path('routes/console.php');
     })
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        $middleware->alias([
+            'status.check' => CheckAccountStatus::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
